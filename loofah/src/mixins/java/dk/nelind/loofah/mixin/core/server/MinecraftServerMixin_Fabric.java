4201764e44736d246a2b4c5d28a2694cf9360694
nelind <nel.n.lindberg@gmail.com>
2024-11-08 17:53:28 +0100

    Make sure Loofah's server mod name overrides SpongeCommon's

--- loofah/src/mixins/java/dk/nelind/loofah/mixin/core/server/MinecraftServerMixin_Fabric.java
+++ loofah/src/mixins/java/dk/nelind/loofah/mixin/core/server/MinecraftServerMixin_Fabric.java
@@ -29,7 +29,8 @@ import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(MinecraftServer.class)
+// Lower the priority to override the server mod name from SpongeCommon
+@Mixin(value = MinecraftServer.class, priority = 1102)
 public abstract class MinecraftServerMixin_Fabric implements FabricServer {
     /**
      * @author Nelind
